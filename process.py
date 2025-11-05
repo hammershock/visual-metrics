b90e0718847a97aa238db00b1e498316765ac309
hammershock <hammershock@sjtu.edu.cn>
2025-11-05 15:10:34 +0800

    feat(dataloader):  a sketch of dataloader

---
+++ process.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+from PIL import Image
+
+def dataloader(base_dir="/temp/rey/stylagent/images/omniconsistency"):
+    for category in os.listdir(base_dir):
+        category_path = os.path.join(base_dir, category)
+        print(category_path)
+        # load jsonl:
+        analysis_path = os.path.join(category_path, "analysis.jsonl")  # analysis.jsonl | val.jsonl
+        with open(analysis_path, "r", encoding="utf-8") as f:
+            for line in f:
+                line = line.strip()
+                assert len(line) > 0
+                data = json.loads(line)
+        
+                cnt_path = base_dir + data['cnt']
+                ref_path = base_dir + data['ref']
+                yield Image.open(cnt_path), Image.open(ref_path)
+                # yield cnt_path, ref_path
+        
+
+if __name__ == "__main__":
+    for cnt, ref in dataloader():
+        pass
+        
